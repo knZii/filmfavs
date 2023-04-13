52ff055f745ef1c7a86ed3f5ab1cf483a10587aa
knzi <mahanbst85@gmail.com>
2023-04-13 04:49:43 -0700

    Init nextjs and matine and make simple ui with them

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # filmfavs
-site that can suggest best film for you based on your interests 
+
+site that can suggest best film for you based on your interests
+
+## Framework/Library I use
+
+- Nextjs _Java script (React) library_
+- Prismadb _Make it easier to work with db_
+- mantine _ui framework for nextjs_
+
+## How to use it
+
+1. clone the repo
+2. run `npm install`
+3. run `npm run dev`
---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import {Container, Text, createStyles, rem, Button} from '@mantine/core'
+
+
+const useStyles = createStyles(theme => ({
+  wrapper: {
+    position: 'relative',
+    boxSizing: 'border-box',
+    backgroundColor: theme.colorScheme == 'dark' ? theme.colors.dark : theme.white
+  },
+  inner: {
+    position: 'relative',
+    paddingTop: rem(200),
+  },
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: rem(60),
+    fontWeight: 900,
+    lineHeight: 1.1,
+  },
+  description: {
+    fontSize: rem(26),
+    marginTop: rem(90)
+  },
+  button: {
+    marginTop: rem(45)
+  }
+}))
+
+export default () => {
+  const {classes} = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <Container className={classes.inner}>
+      <h1 className={classes.title}><Text component='span' variant='gradient' gradient={{from: 'teal', to: 'indigo', deg: 25}}>FilmFaves</Text> Your Personalized Movie Recommendation Engine</h1>
+      <Text className={classes.description} color='dimmed'>Find Your Next Favorite Movie with MovieFavs: Get Customized Movie Recommendations Based on Your Tastes and Preferences</Text>
+      <Button
+            size="xl"
+            className={classes.button}
+            variant="gradient"
+            gradient={{ from: 'teal', to: 'blue' }}
+          >
+            Find your movie !
+          </Button>
+      </Container>
+    </div>
+  )
+}
