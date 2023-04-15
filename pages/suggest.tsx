fac3b318dd80055cf91558d36799963d284a57e4
knzi <mahanbst85@gmail.com>
2023-04-15 06:42:00 -0700

    make it a bit reponsive

--- pages/suggest.tsx
+++ pages/suggest.tsx
@@ -1,4 +1,4 @@
-import {Container, createStyles, rem, Text, Center, Stepper, Group, Button, ActionIcon, useMantineColorScheme, MultiSelect} from '@mantine/core'
+import {Container, createStyles, rem, Text, Center, Stepper, Group, Button, ActionIcon, useMantineColorScheme, MultiSelect, Flex} from '@mantine/core'
 import {IconMoon, IconSunHigh} from '@tabler/icons-react'
 import {useState} from 'react'
 
@@ -6,11 +6,13 @@ const useStyles = createStyles(theme => ({
     wrapper: {
         position: 'relative',
         boxSizing: 'border-box',
-        backgroundColor: theme.colorScheme == 'dark' ? theme.colors.dark[7] : theme.white
+        backgroundColor: theme.colorScheme == 'dark' ? theme.colors.dark[7] : theme.white,
+        height: '90vh'
       },
       inner: {
         position: 'relative',
         paddingTop: rem(180),
+        height: '100%',
         [theme.fn.smallerThan('sm')]: {
           paddingTop: rem(80)
         }
@@ -55,27 +57,31 @@ export default () => {
              <ActionIcon radius={15} variant='light' color={colorScheme =='light'?'gray.9':'teal'} size={50} mt={30} ml={30} onClick={() => toggleColorScheme()}>
         {colorScheme == 'light'?<IconMoon />:<IconSunHigh />}
         </ActionIcon>
-            <Container className={classes.inner}>
-                <Center>
-                <h1 className={classes.title}>Let's find your <Text component='span' variant='gradient' gradient={{from:'teal.4', to:'blue.7', deg:45}}>Fav</Text> movie together !</h1>
-                </Center>
-                <Stepper className={classes.stepper} active={active} allowNextStepsSelect={false} color='teal.6'>
-                    <Stepper.Step label="Genre" description="What Genre you like?">
-                      <Center>
-                      <MultiSelect value={favgenres} onChange={setFavgenres}  transitionProps={{ duration: 150, transition: 'pop-top-left', timingFunction: 'ease' }} variant='filled' className={classes.genreselect} searchable nothingFound="Nothing found!"  placeholder='Select genres you like up to 3!' data={genres} maxSelectedValues={3}/>
-                      </Center>
-                    </Stepper.Step>
-                    <Stepper.Step label="Mood" description="What is your mood?">
+            <Container className={classes.inner} >
+                <Flex direction='column' justify='space-between' style={{height: '70%'}}>
+                  <Flex direction='column'>
+                    <Center>
+                      <h1 className={classes.title}>Let's find your <Text component='span' variant='gradient' gradient={{from:'teal.4', to:'blue.7', deg:45}}>Fav</Text> movie together !</h1>
+                    </Center>
+                  <Stepper breakpoint='sm' className={classes.stepper} active={active} allowNextStepsSelect={false} color='teal.6'>
+                      <Stepper.Step label="Genre" description="What Genre you like?">
+                        <Center>
+                        <MultiSelect value={favgenres} onChange={setFavgenres}  transitionProps={{ duration: 150, transition: 'pop-top-left', timingFunction: 'ease' }} variant='filled' className={classes.genreselect} searchable nothingFound="Nothing found!"  placeholder='Select genres you like up to 3!' data={genres} maxSelectedValues={3}/>
+                        </Center>
+                      </Stepper.Step>
+                      <Stepper.Step label="Mood" description="What is your mood?">
 
-                    </Stepper.Step>
-                    <Stepper.Step label="Actor/Actress" description="What actor/actress You like?">
+                      </Stepper.Step>
+                      <Stepper.Step label="Actor/Actress" description="What actor/actress You like?">
 
-                    </Stepper.Step>
-                </Stepper>
-                <Group position="center" mt="xl">
-                <Button variant="default" onClick={prevStep}>Back</Button>
-                <Button onClick={nextStep} color='teal.6'>Next step</Button>
-                </Group>
+                      </Stepper.Step>
+                  </Stepper>
+                  </Flex>
+                  <Group position="center" mt="xl">
+                  <Button variant="default" onClick={prevStep}>Back</Button>
+                  <Button onClick={nextStep} color='teal.6'>Next step</Button>
+                  </Group>
+                </Flex>
             </Container>
         </div>
     )
